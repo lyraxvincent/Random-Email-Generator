06650acf218804047111b4d0970585e12921e78a
lyraxvincent <njongevincent@gmail.com>
2020-03-02 10:14:02 +0300

    Initial commit

--- passgen.py
+++ passgen.py
@@ -1,17 +1,17 @@
 #This is a script for generating strong random passwords
-#Length of generated password is 8
+#Length of generated password is 8 characters
 
 import random
 
 alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-characters = ['!', '@', '_', '#', '&']
+punc = ['!', '@', '_', '#', '&']
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 random.shuffle(alphabets)
-random.shuffle(characters)
+random.shuffle(punc)
 random.shuffle(digits)
 
-print('{}{}{}{}'.format(''.join(alphabets[:2]), ''.join(alphabets[-2:]).upper(), ''.join(digits[:3]), characters[0]))
+print('{}{}{}{}'.format(''.join(alphabets[:2]), ''.join(alphabets[-2:]).upper(), ''.join(digits[:3]), punc[0]))
 
 '''
 #This is if the user wants to generate several passwords to choose from...

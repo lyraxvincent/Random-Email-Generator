1976c314fc05f6be3ddf8d1824d19fd9b58a86e7
lyraxvincent <njongevincent@gmail.com>
2022-01-30 21:12:30 +0300

    improve code readability

--- passwdGenerator.py
+++ passwdGenerator.py
@@ -7,12 +7,14 @@ punc = ['!', '@', '_', '#', '&']
 digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 def generate_passwds(n=1):
+
     passwds = []
 
     for i in range(n):
         random.shuffle(alphabets); random.shuffle(punc); random.shuffle(digits)
-        passwds.append('{}{}{}{}'.format(''.join(alphabets[:2]), ''.join(alphabets[-2:]).upper(),
-                       ''.join(digits[:3]), punc[0]))
+        passwds.append(
+            '{}{}{}{}'.format(''.join(alphabets[:2]), ''.join(alphabets[-2:]).upper(), ''.join(digits[:3]), punc[0])
+        )
 
     return passwds
 
